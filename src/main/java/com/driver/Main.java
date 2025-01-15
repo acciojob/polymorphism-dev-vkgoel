926b2a0158f99c46b2298ac4178ac9ac3e2368c2
dev-vkgoel <vkgoel1505@gmail.com>
2025-01-15 14:15:20 +0530

    solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+
+
+        System.out.println(p.Product(5,6));
+        System.out.println(p.Product(5,6,9));
+        System.out.println(p.Product(5.5,6.5));
+    }
 
 }
---
+++ src/main/java/com/driver/Product.java
@@ -0,0 +1,13 @@
+package com.driver;
+
+public class Product {
+    public int Product(int x, int y){
+        return x + y;
+    }
+    public int Product(int x, int y, int z){
+        return x + y + z;
+    }
+    public double Product(double x, double y){
+        return x + y;
+    }
+}
